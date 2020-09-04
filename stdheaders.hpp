2b1ba5c6e1c8529bb6da1ef00d7c60b86ded6eec
Terry M. Poulin <BigBoss1964@gmail.com>
2020-09-03 20:55:39 -0400

    Add the headers.
    Which project did I pull these from? SxE. I've used more or less the same
    in multiple code bases of mine.

---
+++ stdheaders.hpp
@@ -0,0 +1,161 @@
+#ifndef PROJECT_STDHEADERS__H
+#define PROJECT_STDHEADERS__H
+
+/** Standard C++ headers.
+ *
+ * These are included in groups as noted by https://en.cppreference.com/w/cpp/header and guarded by PROJECT_CXXnn.
+ */
+#include <PROJECT_/cxxversion.hpp>
+
+/* Utilities library */
+#include <cstdlib>
+#include <csignal>
+#include <csetjmp>
+#include <cstdarg>
+#include <typeinfo>
+#if PROJECT_CXX11
+#include <typeindex>
+#include <type_traits>
+#endif
+#include <bitset>
+#include <functional>
+#include <utility>
+#include <ctime>
+#if PROJECT_CXX11
+#include <chrono>
+#endif
+#include <cstddef>
+#if PROJECT_CXX11
+#include <initializer_list>
+#include <tuple>
+#endif
+#if PROJECT_CXX17
+#include <any>
+#include <optional>
+#include <variant>
+#endif
+
+/* Dynamic memory maangement. */
+#include <new>
+#include <memory>
+#if PROJECT_CXX11
+#include <scoped_allocator>
+#endif
+#if PROJECT_CXX17 && (!PROJECT_GCC_VERSION || PROJECT_GCC_VERSION > 90100)
+#include <memory_resource>
+#endif
+
+/* Numeric limits. */
+#include <climits>
+#include <cfloat>
+#if PROJECT_CXX11
+#include <cstdint>
+#include <cinttypes>
+#endif
+#include <limits>
+
+/* Error handling. */
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#if PROJECT_CXX11
+#include <system_error>
+#endif
+#include <cerrno>
+
+/* Strings library. */
+#include <cctype>
+#include <cwctype>
+#include <cstring>
+#include <cwchar>
+#if PROJECT_CXX11
+#include <cuchar>
+#endif
+#include <string>
+#if PROJECT_CXX17
+#include <string_view>
+#include <charconv>
+#endif
+
+/* Containers library. */
+#if PROJECT_CXX11
+#include <array>
+#endif
+#include <vector>
+#include <deque>
+#include <list>
+#if PROJECT_CXX11
+#include <forward_list>
+#endif
+#include <set>
+#include <map>
+#if PROJECT_CXX11
+#include <unordered_set>
+#include <unordered_map>
+#endif
+#include <stack>
+#include <queue>
+
+/* Iterators library. */
+#include <iterator>
+
+/* Algorithms library. */
+#include <algorithm>
+#if PROJECT_CXX17 && (!PROJECT_GCC_VERSION || PROJECT_GCC_VERSION > 90100)
+#include <execution>
+#endif
+
+/* Numerics library. */
+#include <cmath>
+#include <complex>
+#include <valarray>
+#if PROJECT_CXX11
+#include <random>
+#endif
+#include <numeric>
+#if PROJECT_CXX11
+#include <ratio>
+#include <cfenv>
+#endif
+
+/* Input/output library. */
+#include <iosfwd>
+#include <ios>
+#include <istream>
+#include <ostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <streambuf>
+#include <cstdio>
+
+/* Localization library. */
+#include <locale>
+#include <clocale>
+
+/* Regular Expressions library. */
+#if PROJECT_CXX11
+#include <regex>
+#endif
+
+/** Atomic Operations library. */
+#if PROJECT_CXX11
+#include <atomic>
+#endif
+
+/* Thread support library. */
+#if PROJECT_CXX11
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <future>
+#include <condition_variable>
+#endif
+
+/* Filesystem library. */
+#if PROJECT_CXX17
+#include <filesystem>
+#endif
+
+#endif // PROJECT_STDHEADERS__H
